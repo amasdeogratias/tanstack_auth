7bee54885d98cb9253a86d811316fbaa01b3d66f
DeogratiasAmas <deogratiasprog@gmail.com>
2026-03-12 12:43:13 +0300

    Added enums for categories in posts schema

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -1,5 +1,30 @@
-import { mysqlTable, varchar, int, datetime } from "drizzle-orm/mysql-core";
+import {
+  mysqlTable,
+  varchar,
+  int,
+  datetime,
+  mysqlEnum,
+} from "drizzle-orm/mysql-core";
 import { sql } from "drizzle-orm";
+
+export enum Category {
+  Agriculture = "Agriculture",
+  Business = "Business",
+  Education = "Education",
+  Entertainment = "Entertainment",
+  Art = "Art",
+  Investment = "Investment",
+  Uncategorized = "Uncategorized",
+  Weather = "Weather",
+}
+
+export function enumToMysqlEnum<T extends Record<string, any>>(
+  myEnum: T,
+): [T[keyof T], ...T[keyof T][]] {
+  return Object.values(myEnum).map((value: any) => `${value}`) as any;
+}
+
+export const categoryEnum = mysqlEnum("category", enumToMysqlEnum(Category));
 
 export const users = mysqlTable("users", {
   id: int("id").primaryKey().autoincrement(),
@@ -21,14 +46,17 @@ export const sessions = mysqlTable("sessions", {
   createdAt: datetime("created_at").default(sql`CURRENT_TIMESTAMP`),
 });
 
-
 export const posts = mysqlTable("posts", {
   id: int("id").primaryKey().autoincrement(),
   user_id: int("user_id")
     .notNull()
     .references(() => users.id),
   title: varchar("title", { length: 255 }).notNull(),
+  category: categoryEnum,
   content: varchar("content", { length: 255 }).notNull(),
+  image: varchar("image", { length: 255 }),
   createdAt: datetime("created_at").default(sql`CURRENT_TIMESTAMP`),
-  updatedAt: datetime("updated_at").default(sql`CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP`),
-})
+  updatedAt: datetime("updated_at").default(
+    sql`CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP`,
+  ),
+});
